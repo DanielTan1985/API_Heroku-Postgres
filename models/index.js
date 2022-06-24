664dec49c3b728949af8bae06c9d3e7837f2ed15
DanielTan1985 <97973757+DanielTan1985@users.noreply.github.com>
2022-06-24 17:02:33 +0800

    Update index.js

--- models/index.js
+++ models/index.js
@@ -2,6 +2,8 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize('d7r4o5vvh2upn9', 'bimumvggwjprjc', '1d670fcd93a18be2cefeae429e845731e01788cd626b2e8382ec7798467bf7cf', {
     host: 'ec2-54-80-122-11.compute-1.amazonaws.com',
+const sequelize = new Sequelize('d7p6efp7ubbba7', 'nhpsdmwpjtpdkq', '6b298c5d08e46bce2428e86bd39448c707b79db3529cc5780cc8126c0be315c6', {
+    host: 'ec2-54-211-255-161.compute-1.amazonaws.com',
     dialect: 'postgres',
     dialectOptions:{
       ssl: {
