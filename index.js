00a96a1c909e7dbc842235fbd8ebb149d1a8e24a
unknown <danieltan85@gmail.com>
2022-06-29 10:59:51 +0800

    Cors Setting 1

--- index.js
+++ index.js
@@ -15,9 +15,16 @@ app.use(cors(corsOptions));
 // app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.send("Welcome to Node.js Backend API, please manually select routes as stipulated:  /public  /user");
+  next()
 });
+
+// app.get('/', (req, res) => {
+//   res.send("Welcome to Node.js Backend API, please manually select routes as stipulated:  /public  /user");
+// });
 
 
 
