320fc2fd336a9ef5bf90346c514415a37bc2d40e
unknown <danieltan85@gmail.com>
2022-07-04 15:27:34 +0800

    Configure Cors

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cors(corsOptions));
 
 
 app.all('/', cors(corsOptions), function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://backendpersonalapi.herokuapp.com");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.send("Welcome to Node.js Backend API, please manually select routes as stipulated:  /public  /user");
   next()
