152b334f8af389e9272b02934b8037a5ae8e213f
unknown <danieltan85@gmail.com>
2022-07-04 15:22:39 +0800

    Configure Cors

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 
-app.all('/', function(req, res, next) {
+app.all('/', cors(corsOptions), function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.send("Welcome to Node.js Backend API, please manually select routes as stipulated:  /public  /user");
