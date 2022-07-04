5618ce27f62384cfe0f0b7c79c513f71e5cf8894
unknown <danieltan85@gmail.com>
2022-07-04 13:58:30 +0800

    Configure Cors

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const path = require('path');
 const cors = require('cors');
 
 
+
+// app.use(cors());
+app.use(bodyParser.json());
+
+// app.get('/', (req, res) => {
+//   res.send("Welcome to Node.js Backend API, please manually select routes as stipulated:  /public  /user");
+// });
+
+
+
 var corsOptions = {
   // origin: "https://gentle-fortress-35413.herokuapp.com",
   origin: "http://localhost:3000",
@@ -12,8 +22,7 @@ var corsOptions = {
   // optionSuccessStatus:200,
 };
 app.use(cors(corsOptions));
-// app.use(cors());
-app.use(bodyParser.json());
+
 
 app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -22,13 +31,27 @@ app.all('/', function(req, res, next) {
   next()
 });
 
-// app.get('/', (req, res) => {
-//   res.send("Welcome to Node.js Backend API, please manually select routes as stipulated:  /public  /user");
-// });
-
 
 
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}...`);
   
 });
+
+
+
+// var corsOptions = {
+//   // origin: "https://gentle-fortress-35413.herokuapp.com",
+//   origin: "http://localhost:3000",
+//   // credentials: true,
+//   // optionSuccessStatus:200,
+// };
+// app.use(cors(corsOptions));
+
+
+// app.all('/', function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   res.send("Welcome to Node.js Backend API, please manually select routes as stipulated:  /public  /user");
+//   next()
+// });
